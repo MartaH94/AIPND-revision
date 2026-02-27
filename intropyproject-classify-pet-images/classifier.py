55983f2581d5c0f543135d1abd8e4253216a77ac
Marta Haraj <marta.haraj@accenture.com>
2026-02-27 11:19:43 +0100

    Refactor classifier function to improve image loading and tensor handling; remove deprecated Variable usage and streamline model evaluation

--- intropyproject-classify-pet-images/classifier.py
+++ intropyproject-classify-pet-images/classifier.py
@@ -1,10 +1,9 @@
 import ast
 
+import torch
 import torchvision.models as models
 import torchvision.transforms as transforms
 from PIL import Image
-from torch import __version__
-from torch.autograd import Variable
 
 resnet18 = models.resnet18(pretrained=True)
 alexnet = models.alexnet(pretrained=True)
@@ -18,8 +17,8 @@ with open('imagenet1000_clsid_to_human.txt') as imagenet_classes_file:
 
 
 def classifier(img_path, model_name):
-    # load the image
-    img_pil = Image.open(img_path)
+    # load the image (and convert to RGB)
+    img_pil = Image.open(img_path).convert('RGB')
 
     # define transforms
     preprocess = transforms.Compose([
@@ -31,47 +30,52 @@ def classifier(img_path, model_name):
     ])
 
     # preprocess the image
-    img_tensor = preprocess(img_pil)
+    input_tensor = preprocess(img_pil)
 
     # resize the tensor (add dimension for batch)
-    img_tensor.unsqueeze_(0)
+    input_tensor.unsqueeze_(0)
 
     # wrap input in variable, wrap input in variable - no longer needed for
     # v 0.4 & higher code changed 04/26/2018 by Jennifer S. to handle PyTorch upgrade
-    pytorch_ver = __version__.split('.')
+    # pytorch_ver = __version__.split('.')
 
-    # pytorch versions 0.4 & hihger - Variable depreciated so that it returns
-    # a tensor. So to address tensor as output (not wrapper) and to mimic the
-    # affect of setting volatile = True (because we are using pretrained models
-    # for inference) we can set requires_gradient to False. Here we just set
-    # requires_grad_ to False on our tensor
-    if int(pytorch_ver[0]) > 0 or int(pytorch_ver[1]) >= 4:
-        img_tensor.requires_grad_(False)
+    # # pytorch versions 0.4 & hihger - Variable depreciated so that it returns
+    # # a tensor. So to address tensor as output (not wrapper) and to mimic the
+    # # affect of setting volatile = True (because we are using pretrained models
+    # # for inference) we can set requires_gradient to False. Here we just set
+    # # requires_grad_ to False on our tensor
+    # if int(pytorch_ver[0]) > 0 or int(pytorch_ver[1]) >= 4:
+    #     img_tensor.requires_grad_(False)
 
-    # pytorch versions less than 0.4 - uses Variable because not-depreciated
-    else:
-        # apply model to input
-        # wrap input in variable
-        data = Variable(img_tensor, volatile=True)
+    # # pytorch versions less than 0.4 - uses Variable because not-depreciated
+    # else:
+    #     # apply model to input
+    #     # wrap input in variable
+    #     data = Variable(img_tensor, volatile=True)
 
     # apply model to input
-    model = model_dict[model_name]
+    model = model_dict[model_name].eval()
+
+    with torch.no_grad():
+        output = model(input_tensor)
 
     # puts model in evaluation mode
     # instead of (default)training mode
-    model = model.eval()
+    # model = model.eval()
 
-    # apply data to model - adjusted based upon version to account for
-    # operating on a Tensor for version 0.4 & higher.
-    if int(pytorch_ver[0]) > 0 or int(pytorch_ver[1]) >= 4:
-        output = model(img_tensor)
+    # # apply data to model - adjusted based upon version to account for
+    # # operating on a Tensor for version 0.4 & higher.
+    # if int(pytorch_ver[0]) > 0 or int(pytorch_ver[1]) >= 4:
+    #     output = model(img_tensor)
 
-    # pytorch versions less than 0.4
-    else:
-        # apply data to model
-        output = model(data)
+    # # pytorch versions less than 0.4
+    # else:
+    #     # apply data to model
+    #     output = model(data)
 
     # return index corresponding to predicted class
-    pred_idx = output.data.numpy().argmax()
+    pred_idx = output.argmax(dim=1).item()
+
+    # pred_idx = output.data.numpy().argmax()
 
     return imagenet_classes_dict[pred_idx]
