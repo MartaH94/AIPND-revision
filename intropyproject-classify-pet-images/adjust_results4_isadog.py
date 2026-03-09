470915df06a087827d25438034bf6e27088d5306
Marta Haraj <marta.haraj@accenture.com>
2026-03-09 11:59:56 +0100

    Add logic to extend results dictionary when classifier label is a dog

--- intropyproject-classify-pet-images/adjust_results4_isadog.py
+++ intropyproject-classify-pet-images/adjust_results4_isadog.py
@@ -141,7 +141,7 @@ def adjust_results4_isadog(results_dic, dogfile):
             # Classifier Label IS image of Dog (e.g. found in dognames_dic)
             # appends (0, 1)because only Classifier labe is a dog
             if results_dic[key][1] in dognames_dic:
-                pass
+                results_dic[key].extend((0, 1))
 
             # TODO: 4e. REPLACE pass BELOW with CODE that adds the following to
             #           results_dic dictionary for the key indicated by the
